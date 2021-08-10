d0c6abefa2d30b4e9e9f00ab5ab0d46e0d7fef42
Marcelo - Commarc <commarc@losinox.com.br>
2021-08-10 00:01:27 -0300

    Created async connector called WK.ConnectorAsync. It's new one to allow async connections on SQL Server DataBases.

---
+++ WK.ConnectorAsync/ConnectAsync.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace WK.ConnectorAsync
+{
+    public class ConnectAsync : IDisposable
+    {
+        private String _connectionStringPropertyName = "DefaultConnection";
+
+        public String ConnectionStringPropertyName
+        { 
+            get { return _connectionStringPropertyName; }
+            set { _connectionStringPropertyName = value; }
+        }
+
+        public IDbConnection Connection { get; }
+
+        public IDbTransaction Transaction { get; set; }
+        
+        public ConnectAsync(IConfiguration configuration)
+        {
+            Connection = new SqlConnection(configuration.GetConnectionString(this.ConnectionStringPropertyName));
+        }
+
+        public void Dispose() => Connection?.Dispose();
+
+    }
+}
---
+++ WK.ConnectorAsync/DataAsync.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+
+namespace WK.ConnectorAsync
+{
+    public class DataAsync : IDisposable
+    {
+
+        private ConnectAsync _dbConnection;
+
+        public DataAsync(ConnectAsync dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Object> GetDataByPK(String Command)
+        {
+            Object obj = new();
+
+            using var conn = _dbConnection.Connection;
+            try
+            {
+                conn.Open();
+                obj = await conn.ExecuteScalarAsync<Object>(Command);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(String.Format("Failure detected in [{0}] \r\nDetail: {1}", this.ToString(), ex.Message));
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+            return obj;
+            
+        }
+    }
+}
